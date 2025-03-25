0e924cafc7abe1dc848c2327864ee9c82502d338
Daniel Fredes <danifredesok@gmail.com>
2025-03-25 13:33:01 -0300

    add ignore y punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,10 @@
 
 int main(void)
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
 
+    int numero = 1, *puntero = &numero;
+
+    printf("contenido del puntero: %d\n y la direccion del puntero es: %p\n Direccion de la memoria de la variable %p\n Direccion de memoria del puntero %p\n Tamano de memoria utilizado por la variable usando Sizeof %d\n", *puntero, puntero, &numero, &puntero), sizeof(puntero);
     return 0;
 }
